edcd70dca53c5cefd43a170c812d0dff731b4a78
JabirMJ1 <jabirmj1@gmail.com>
2021-11-30 19:57:45 +0530

    renamed

---
+++ 1.sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int first, second, sum;
+	printf("Enter the first number: \n");
+	scanf("%d", &first);
+	printf("\nEnter the second number: \n");
+	scanf("%d", &second);
+	sum =  first + second;
+	printf("\nSum = %d\n", sum);
+	return(0);
+}
